b549ff21620a975677bf1156245d1616fa4c5d06
@flavio.mezei <flavio.mezei@paylivre.com>
2023-03-13 15:16:18 -0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from "./components/layout/Container";
+
+function App() {
+  return (
+      <Router>
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/company">Empresa</Link>
+            <Link to="/contact">Contato</Link>
+            <Link to="/newproject">Novo Projeto</Link>
+        </div>
+          <Switch>
+              <Container customClass="min-height">
+                  <Route exact path="/">
+                        <Home />
+                  </Route>
+                  <Route exact path="/company">
+                      <Company />
+                  </Route>
+                  <Route exact path="/contact">
+                      <Contact />
+                  </Route>
+                  <Route exact path="/newproject">
+                      <NewProject />
+                  </Route>
+                </Container>
+          </Switch>
+      <p>Footer</p>
+    </Router>
+  );
+}
+
+export default App;
