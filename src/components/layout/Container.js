661dd34f977844982927cdaa2e7670889dd8a937
@flavio.mezei <flavio.mezei@paylivre.com>
2023-03-13 16:48:53 -0300

    CS-fix

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Container.module.css';
+
 function Container(props) {
     return (
     <div className={`${styles.container} ${styles[props.customClass]}`}>
