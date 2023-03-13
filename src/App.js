1cd9a85627e3b40ce1e732d9f1e3d502700ccace
@flavio.mezei <flavio.mezei@paylivre.com>
2023-03-13 17:59:25 -0300

    navbar, footer, layout and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Projects from "./components/pages/Projects";
 
 function App() {
   return (
       <Router>
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/company">Empresa</Link>
-            <Link to="/contact">Contato</Link>
-            <Link to="/newproject">Novo Projeto</Link>
-        </div>
+        <Navbar />
           <Switch>
               <Container customClass="min-height">
                   <Route exact path="/">
                         <Home />
+                  </Route>
+                  <Route exact path="/projects">
+                      <Projects />
                   </Route>
                   <Route exact path="/company">
                       <Company />
@@ -30,7 +31,7 @@ function App() {
                   </Route>
                 </Container>
           </Switch>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
